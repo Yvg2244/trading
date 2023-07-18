0a737ae0ca56d68191865a3d05c7534fcea00481
Yvg2244 <yvgupta24@gmail.com>
2023-07-19 03:07:00 +0530

    login resiter added

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [loginError, setLoginError] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:5000/login-user", {
+      method: "POST",
+      crossDomain: true,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        email: loginEmail,
+        password: loginPassword,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.data);
+        if (data.status === "ok") {
+          window.localStorage.setItem("token", data.token);
+          window.localStorage.setItem("email", data.email);
+          window.localStorage.setItem("userdata", JSON.stringify(data.data));
+          window.location.href = "./userDetails";
+        } else {
+          setError("Invalid email or password");
+        }
+      })
+      .catch((loginError) => {
+        setLoginError("An error occurred during login");
+      });
+  };
   return (
     <div className="navbar fixed top-0 z-10 bg-base-100">
       <div className="navbar-start">
+        {/* small screen menu */}
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg
@@ -37,11 +74,13 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
+        {/* heading */}
         <a className="btn btn-ghost normal-case text-xl">
           Instacorp Securities
         </a>
       </div>
-      <div className="navbar-center hidden lg:flex">
+      {/* large screen navbar */}
+      {/* <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal gap-4 px-1">
           <li>
             <Link to="/">Paper Trading</Link>
@@ -53,9 +92,115 @@ const Navbar = () => {
             <Link to="/marketplace">Trading</Link>
           </li>
         </ul>
-      </div>
+      </div> */}
+
+      {/* Login/Register Modal */}
       <div className="navbar-end">
-        <a className="btn">Logout</a>
+        {isLoggedIn ? (
+          <button className="btn">Logout</button>
+        ) : (
+          <button className="btn" onClick={() => window.loginModal.showModal()}>
+            Login
+          </button>
+        )}
+
+        {/* login model`` */}
+        <dialog id="loginModal" className="modal ">
+          <form method="dialog" className="modal-box flex flex-col gap-2">
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+            <h4 className="font-bold my-4 flex flex-col text-md">Login</h4>
+            <label className="label">
+              <span className="label-text text-md font-bold">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter email"
+              onChange={() => {
+                (e) => {
+                  setLoginEmail(e.target.value);
+                };
+              }}
+              className="input input-bordered w-full max-w-xs"
+            />
+            <label className="label">
+              <span className="label-text text-md font-bold">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter password"
+              onChange={() => {
+                (e) => {
+                  setLoginPassword(e.target.value);
+                };
+              }}
+              className="input input-bordered w-full max-w-xs"
+            />
+            <Link
+           
+              onClick={handleLogin}
+              className="btn w-full "
+            >
+              Sign In
+            </Link>
+            <p>Don't have a account?</p>
+            <a
+              onClick={() => window.registerModal.showModal()}
+              className="btn w-full "
+            >
+              Register
+            </a>
+            {/* register model */}
+            <dialog id="registerModal" className="modal ">
+              <form method="dialog" className="modal-box  ">
+                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                  ✕
+                </button>
+                <h4 className="font-bold my-4 flex flex-col text-md">
+                  Register
+                </h4>
+                <label className="label">
+                  <span className="label-text text-md font-bold">
+                    Full Name
+                  </span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter your full name"
+                  className="input input-bordered w-full max-w-xs"
+                />
+                <label className="label">
+                  <span className="label-text text-md font-bold">Email</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter your email"
+                  className="input input-bordered w-full max-w-xs"
+                />
+                <label className="label">
+                  <span className="label-text text-md font-bold">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Enter your password"
+                  className="input input-bordered w-full max-w-xs"
+                />
+                <label className="label">
+                  <span className="label-text text-md font-bold">Username</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter your username"
+                  className="input input-bordered w-full max-w-xs"
+                />
+                <a className="btn w-full my-4">Register</a>
+                <p>Already have a account?</p>
+                <button className="btn my-4 w-full">Sign In</button>
+              </form>
+            </dialog>
+          </form>
+        </dialog>
       </div>
     </div>
   );
--- frontend/src/Components/PaperTrading.jsx
+++ frontend/src/Components/PaperTrading.jsx
@@ -4,18 +4,192 @@ const PaperTrading = () => {
   const [time, setTime] = useState("06:20:56");
   const [nifty, setNifty] = useState("19,711");
   const [bankNifty, setBankNifty] = useState("45,449");
+  // =============================================================================
+  // const [userData, setUserData] = useState("");
+  // const [mainData, setMainData] = useState({
+  //   username: "",
+  //   password: "",
+  //   totp: "",
+  //   userId: "",
+  //   apiKey: "",
+  //   secretKey: "",
+  //   broker: "", // Add the selected dropdown value
+  // });
+  // const [updatedMainData, setUpdatedMainData] = useState(
+  //   JSON.parse(window.localStorage.getItem("userdata")).BrokerList
+  // );
+  // const [selectedOption, setSelectedOption] = useState("");
+  // const [showForm, setShowForm] = useState(false);
+  // const [show, setShow] = useState(false);
+  // const [password, setPassword] = useState("");
+  // const [totp, setTotp] = useState("");
+  // const [userId, setUserId] = useState("");
+  // const [apiKey, setApiKey] = useState("");
+  // const [secretKey, setSecretKey] = useState("");
+  // const [formData, setFormData] = useState([]);
 
+  // //  console.log(data)
+  // let value;
+  // const handleOptionChange = (e) => {
+  //   setSelectedOption(e.target.value);
+  //   setShowForm(true);
+  // };
+
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // useEffect(() => {
+  //   // Check if the user is logged in
+  //   const token = window.localStorage.getItem("token");
+  //   const email = window.localStorage.getItem("email");
+  //   console.log(email, "email");
+  //   const username = window.localStorage.getItem("username");
+  //   if (token) {
+  //     // User is logged in, fetch user data
+  //     fetch("http://localhost:8000/checkAuth", {
+  //       method: "POST",
+  //       crossDomain: true,
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Accept: "application/json",
+  //         "Access-Control-Allow-Origin": "*",
+  //       },
+  //       body: JSON.stringify({
+  //         token: token,
+  //         email: email,
+  //         username: username,
+  //       }),
+  //     })
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         if (data.status === "ok") {
+  //           // User data fetched successfully
+  //           setUserData(data.data);
+  //           setIsLoggedIn(true);
+  //         } else {
+  //           // Token expired or invalid, clear localStorage and redirect to login
+  //           window.localStorage.clear();
+  //           window.location.href = "./sign-in";
+  //         }
+  //       })
+  //       .catch((error) => {
+  //         console.error("Error fetching user data:", error);
+  //       });
+  //   } else {
+  //     // User is not logged in, redirect to login
+  //     window.location.href = "./login";
+  //   }
+  // }, []);
+  // console.log(userData);
+
+  // if (!isLoggedIn) {
+  //   // Render a loading state or redirect to login
+  //   return <div>Loading...</div>;
+  // }
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   // Handle form submission logic here
+
+  //   // Construct the form data object
+  //   const formDataObj = {
+  //     username: userData.Username,
+  //     password,
+  //     totp,
+  //     userId,
+  //     apiKey,
+  //     secretKey,
+  //     broker: selectedOption, // Add the selected dropdown value
+  //   };
+  //   // setMainData(formDataObj)
+  //   // Create a new array with the existing form data and the new form data object
+
+  //   // Serialize the form data array as a string
+  //   // const formDataString = JSON.stringify(updatedMainData, null, 2);
+
+  //   // Send the updated form data string to the backend
+  //   fetch("http://localhost:8000/userData", {
+  //     method: "POST",
+  //     crossDomain: true,
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Accept: "application/json",
+  //       "Access-Control-Allow-Origin": "*",
+  //     },
+  //     body: JSON.stringify({
+  //       token: window.localStorage.getItem("token"),
+  //       BrokerList: formDataObj, // Send the form data string
+  //     }),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       if (data.status) {
+  //         // setUpdatedMainData(prev=>[...prev, formDataObj])
+  //         setUpdatedMainData((prev) => {
+  //           if (!Array.isArray(prev)) {
+  //             return [formDataObj]; // Set prev to an empty array if it's not already an array
+  //           }
+
+  //           return [...prev, formDataObj];
+  //         });
+  //         console.log("Form data updated successfully");
+  //         // console.log(mainData)
+  //         // setFormData(updatedMainData); // Update the local state with the updated form data
+  //       } else {
+  //         console.error("Error updating form data inside:", data.data);
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error updating form data:", error);
+  //     });
+  //   setShowForm(false);
+  //   setShow(true);
+  // };
+
+  // const handleLogout = () => {
+  //   // Clear token from local storage
+  //   window.localStorage.removeItem("token");
+  //   // Redirect to login page
+  //   window.location.href = "./login";
+  // };
+  // function handleButtonClick(brokerName) {
+  //   fetch("http://localhost:8000/generateToken", {
+  //     method: "POST",
+  //     crossDomain: true,
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Accept: "application/json",
+  //       "Access-Control-Allow-Origin": "*",
+  //     },
+  //     body: JSON.stringify({
+  //       token: window.localStorage.getItem("token"),
+  //       brokerName,
+  //     }),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       if (data.status == true) {
+  //         console.log("congrats yoda");
+  //       } else {
+  //         console.log("failed");
+  //       }
+  //     });
+  // }
+  // =====================================================
   return (
     <div className="flex flex-col gap-4 w-ful h-full pt-[5rem] px-5">
       {/* header - nifty time  */}
       <div className="alert shadow-lg">
         <div className="flex flex-col gap-1">
           <h2 className="font-bold">Paper Trading</h2>
+
           <div className="text-xs">
             Time: <b>{time}</b>, Nifty: <b>{nifty}</b>, BankNifty:{" "}
             <b>{bankNifty}</b>
           </div>
         </div>
+      </div>
+      <div className="w-full flex justify-end">
+        <button className="btn ">Add</button>
       </div>
       {/* main area displays cards */}
       <div className="grid gap-x-2 gap-y-4 grid-cols-12">
